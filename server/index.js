74c0c3885a716b6bcd71644566cddaf0905aa705
Abdule Yousuf Hashi <114812664+abduleyousuf08@users.noreply.github.com>
2023-09-24 12:24:32 +0300

    finally updated

--- server/index.js
+++ server/index.js
@@ -27,9 +27,6 @@ app.use(cors());
 
 //Todo: set uping the socket io server and express server
 const server = createServer(app);
-const io = new Server(8090, {
-   cors: '*',
-});
 
 //Todo:express middlewares
 app.use(express.json());
@@ -40,6 +37,54 @@ app.use(cookieParser());
 app.use('/auth', userRoutes);
 app.use('/chat', chatRoutes);
 app.use('/message', messageRoutes);
+
+const io = new Server({
+   cors: 'http://localhost:3000',
+});
+
+let onlineUsers = [];
+
+io.on('connection', (socket) => {
+   console.log('User connected' + socket.id);
+
+   //? LISTEN TO A CONNECTION
+   socket.on('addNewUser', (userId) => {
+      !onlineUsers.some((user) => user.userId === userId) &&
+         onlineUsers.push({
+            userId,
+            socketId: socket.id,
+         });
+
+      io.emit('getOnlineUsers', onlineUsers);
+   });
+
+   //? Send Message
+   socket.on('sendMessage', (message) => {
+      const userToSend = onlineUsers.find(
+         (user) => user.userId === message.recipientId
+      );
+
+      if (userToSend) {
+         io.to(userToSend.socketId).emit('getMessage', message);
+         io.to(userToSend.socketId).emit('getNotifications', {
+            chat: message.chatId,
+            senderId: message.senderId,
+            message: message.message,
+            isRead: false,
+            date: new Date(Date.now()),
+         });
+      }
+   });
+
+   //Disconnecting user and removing from the online users
+   socket.on('disconnect', () => {
+      console.log('user discounted' + socket.id);
+      onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
+      io.emit('getOnlineUsers', onlineUsers);
+   });
+});
+
+io.listen(8000);
 
 const __dirname = path.resolve();
 
